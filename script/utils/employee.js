d0df0662a07cd34561303a3ad4995d4d8836b5ad
Oscar Mwanandimai <oscar.mwana@gmail.com>
2019-07-01 06:40:24 +0200

    dirty try

--- script/utils/employee.js
+++ script/utils/employee.js
@@ -1,99 +1,134 @@
+const employeeAttributes = {
+  maxLevel: 10,
+  costPerDay: 2
+};
 const employeeValues = [
-    {
-        name: 'Abdullah',
-        gender: 'M'
-    }, {
-        name: 'Oliver',
-        gender: 'M'
-    }, {
-        name: 'Anna',
-        gender: 'F'
-    }, {
-        name: 'Oscar',
-        gender: 'M'
-    }, {
-        name: 'Tim',
-        gender: 'M'
-    }, {
-        name: 'Catherine',
-        gender: 'M'
-    }, {
-        name: 'Karina',
-        gender: 'F'
-    }, {
-        name: 'Andres',
-        gender: 'M'
-    }, {
-        name: 'Alejandro',
-        gender: 'M'
-    }, {
-        name: 'Monica',
-        gender: 'F'
-    }, {
-        name: 'Stella',
-        gender: 'F'
-    }, {
-        name: 'Chance',
-        gender: 'M'
-    }, {
-        name: 'Cezar',
-        gender: 'M'
-    }, {
-        name: 'Sahil',
-        gender: 'M'
-    }, {
-        name: 'Vitor',
-        gender: 'M'
-    }, {
-        name: 'Jessica',
-        gender: 'F'
-    }, {
-        name: 'David',
-        gender: 'M'
-    }, {
-        name: 'Scott',
-        gender: 'M'
-    }, {
-        name: 'Alejandra',
-        gender: 'F'
-    }, {
-        name: 'Luis',
-        gender: 'M'
-    }, {
-        name: 'Maria',
-        gender: 'F'
-    }, {
-        name: 'Marta',
-        gender: 'F'
-    }, {
-        name: 'Julia',
-        gender: 'F'
-    }, {
-        name: 'Sebastian',
-        gender: 'M'
-    }, {
-        name: 'Stefanie',
-        gender: 'F'
-    }, {
-        name: 'Elena',
-        gender: 'F'
-    }, {
-        name: 'Arthur',
-        gender: 'M'
-    }, {
-        name: 'Alberto',
-        gender: 'M'
-    }, {
-        name: 'Pablo',
-        gender: 'M'
-    }, {
-        name: 'Mohammed',
-        gender: 'M'
-    }, {
-        name: 'Paula',
-        gender: 'F'
-    }, {
-        name: 'Wanda',
-        gender: 'F'
-    },
+  {
+    name: "Abdullah",
+    gender: "M"
+  },
+  {
+    name: "Oliver",
+    gender: "M"
+  },
+  {
+    name: "Anna",
+    gender: "F"
+  },
+  {
+    name: "Oscar",
+    gender: "M"
+  },
+  {
+    name: "Tim",
+    gender: "M"
+  },
+  {
+    name: "Catherine",
+    gender: "M"
+  },
+  {
+    name: "Karina",
+    gender: "F"
+  },
+  {
+    name: "Andres",
+    gender: "M"
+  },
+  {
+    name: "Alejandro",
+    gender: "M"
+  },
+  {
+    name: "Monica",
+    gender: "F"
+  },
+  {
+    name: "Stella",
+    gender: "F"
+  },
+  {
+    name: "Chance",
+    gender: "M"
+  },
+  {
+    name: "Cezar",
+    gender: "M"
+  },
+  {
+    name: "Sahil",
+    gender: "M"
+  },
+  {
+    name: "Vitor",
+    gender: "M"
+  },
+  {
+    name: "Jessica",
+    gender: "F"
+  },
+  {
+    name: "David",
+    gender: "M"
+  },
+  {
+    name: "Scott",
+    gender: "M"
+  },
+  {
+    name: "Alejandra",
+    gender: "F"
+  },
+  {
+    name: "Luis",
+    gender: "M"
+  },
+  {
+    name: "Maria",
+    gender: "F"
+  },
+  {
+    name: "Marta",
+    gender: "F"
+  },
+  {
+    name: "Julia",
+    gender: "F"
+  },
+  {
+    name: "Sebastian",
+    gender: "M"
+  },
+  {
+    name: "Stefanie",
+    gender: "F"
+  },
+  {
+    name: "Elena",
+    gender: "F"
+  },
+  {
+    name: "Arthur",
+    gender: "M"
+  },
+  {
+    name: "Alberto",
+    gender: "M"
+  },
+  {
+    name: "Pablo",
+    gender: "M"
+  },
+  {
+    name: "Mohammed",
+    gender: "M"
+  },
+  {
+    name: "Paula",
+    gender: "F"
+  },
+  {
+    name: "Wanda",
+    gender: "F"
+  }
 ];
