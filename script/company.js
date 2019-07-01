82d06867ea3ee02fb60fdcd3b2eced46c6cc6c3b
Oscar Mwanandimai <oscar.mwana@gmail.com>
2019-07-01 06:53:29 +0200

    fix linting

--- script/company.js
+++ script/company.js
@@ -1,101 +1,101 @@
 class Company {
-  constructor({ maxDesks, funds, openingHour, closingHour }) {
-    this.employees = [];
-    this.desks = [];
-    this.maxDesks = maxDesks;
-    this.funds = funds;
-    this.openingHour = openingHour;
-    this.closingHour = closingHour;
-    this.time = {
-      hour: openingHour,
-      day: 0
-    };
+    constructor({ maxDesks, funds, openingHour, closingHour }) {
+        this.employees = [];
+        this.desks = [];
+        this.maxDesks = maxDesks;
+        this.funds = funds;
+        this.openingHour = openingHour;
+        this.closingHour = closingHour;
+        this.time = {
+            hour: openingHour,
+            day: 0
+        };
 
-    this.createDesks();
-  }
-
-  increaseHour() {
-    const { closingHour, openingHour } = this;
-    this.time.hour += 1;
-    if (this.time.hour == closingHour + 1) {
-      // day ended
-      this.time.day += 1;
-      this.time.hour = openingHour;
-
-      if (this.time.day === DAYS.length) {
-        // week ended
-        this.time.day = 0;
-      }
+        this.createDesks();
     }
-  }
 
-  addFunds(funds) {
-    this.funds += funds;
-  }
+    increaseHour() {
+        const { closingHour, openingHour } = this;
+        this.time.hour += 1;
+        if (this.time.hour == closingHour + 1) {
+            // day ended
+            this.time.day += 1;
+            this.time.hour = openingHour;
 
-  takeFunds(funds) {
-    this.funds -= funds;
-  }
-
-  makeDesk(props) {
-    return {
-      productivity: props.productivity,
-      employee: null
-    };
-  }
-
-  createDesks() {
-    for (let i = 0; i < this.maxDesks; i++) {
-      // TODO add different productivities to desks
-      this.desks.push(this.makeDesk({ productivity: 1 }));
+            if (this.time.day === DAYS.length) {
+                // week ended
+                this.time.day = 0;
+            }
+        }
     }
-  }
 
-  addEmployee(employee) {
-    console.log("adding employee");
-    this.employees.push(employee);
-    employee.company = this;
-  }
+    addFunds(funds) {
+        this.funds += funds;
+    }
 
-  findEmptyDesk() {
-    return this.desks.find(desk => !desk.employee);
-  }
+    takeFunds(funds) {
+        this.funds -= funds;
+    }
 
-  doWork() {
-    const totalFunds = this.desks.reduce((acc, desk) => {
-      if (desk.employee) {
-        acc += desk.employee.doWork(this.time);
-      }
-      return acc;
-    }, 0);
-    this.addFunds(totalFunds);
-  }
+    makeDesk(props) {
+        return {
+            productivity: props.productivity,
+            employee: null
+        };
+    }
 
-  startOfDay() {
-    // allocate seating
-    this.employees.forEach(employee => {
-      const emptyDesk = this.findEmptyDesk();
-      if (emptyDesk) {
-        emptyDesk.employee = employee;
-        employee.desk = emptyDesk;
-      }
-    });
-  }
+    createDesks() {
+        for (let i = 0; i < this.maxDesks; i++) {
+            // TODO add different productivities to desks
+            this.desks.push(this.makeDesk({ productivity: 1 }));
+        }
+    }
 
-  endOfDay() {
-    // clear seating
-    this.desks.forEach(desk => {
-      const { employee } = desk;
-      if (employee) {
-        employee.desk = null;
-      }
-      desk.employee = null;
-    });
+    addEmployee(employee) {
+        console.log("adding employee");
+        this.employees.push(employee);
+        employee.company = this;
+    }
 
-    // pay salaries
-    const salariesPaid = this.employees.reduce((acc, employee) => {
-      return acc + employee.salary;
-    }, 0);
-    this.takeFunds(salariesPaid);
-  }
+    findEmptyDesk() {
+        return this.desks.find(desk => !desk.employee);
+    }
+
+    doWork() {
+        const totalFunds = this.desks.reduce((acc, desk) => {
+            if (desk.employee) {
+                acc += desk.employee.doWork(this.time);
+            }
+            return acc;
+        }, 0);
+        this.addFunds(totalFunds);
+    }
+
+    startOfDay() {
+        // allocate seating
+        this.employees.forEach(employee => {
+            const emptyDesk = this.findEmptyDesk();
+            if (emptyDesk) {
+                emptyDesk.employee = employee;
+                employee.desk = emptyDesk;
+            }
+        });
+    }
+
+    endOfDay() {
+        // clear seating
+        this.desks.forEach(desk => {
+            const { employee } = desk;
+            if (employee) {
+                employee.desk = null;
+            }
+            desk.employee = null;
+        });
+
+        // pay salaries
+        const salariesPaid = this.employees.reduce((acc, employee) => {
+            return acc + employee.salary;
+        }, 0);
+        this.takeFunds(salariesPaid);
+    }
 }
